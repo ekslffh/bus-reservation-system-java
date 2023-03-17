3c95989421c1e4661026f08385977efcf67f457b
nasungmin <ekslffh@gmail.com>
2023-03-17 17:15:55 +0900

    기능 동기화

--- src/dao/BusCompanyDAO.java
+++ src/dao/BusCompanyDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,6 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import dto.BusCompanyDTO;
 import util.JDBCTemplate;
+
+
+//SELECT * FROM BUS_COMPANY;
+//SELECT * FROM BUS_COMPANY WHERE C_CODE = '1234';
+//INSERT INTO BUS_COMPANY(C_CODE, C_NAME, C_ADDRESS, C_TELNO) VALUES('1234', '금호고속', '대전 서구 월평동 123', '01084866687');
+//UPDATE BUS_COMPANY SET C_ADDRESS = '대전 만년동 327-16', C_TELNO = '01064875511' WHERE C_CODE = '1234';
+//UPDATE BUS_COMPANY SET C_ADDRESS = '대전 만년동2 327-16' WHERE C_CODE = '1234';
+//UPDATE BUS_COMPANY SET C_TELNO = '01097445511' WHERE C_CODE = '1234';
+//DELETE FROM BUS_COMPANY WHERE C_CODE = '1234';
 
 public class BusCompanyDAO implements DaoIfs<BusCompanyDTO> {
 	private JDBCTemplate util = JDBCTemplate.getInstance();
@@ -44,15 +54,47 @@ public class BusCompanyDAO implements DaoIfs<BusCompanyDTO> {
 		return list;
 	}
 
+	//SELECT * FROM BUS_COMPANY WHERE C_CODE = '1234';
+
 	@Override
 	public BusCompanyDTO findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		BusCompanyDTO dto = new BusCompanyDTO();
+		
+		Connection conn = util.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT * FROM BUS_COMPANY WHERE C_CODE = " + id;
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.executeQuery(sql);
+			
+			rs.next();
+			dto.setCode(rs.getString("C_CODE"));
+			dto.setName(rs.getString("C_NAME"));
+			dto.setAddress(rs.getString("C_ADDRESS"));
+			dto.setTel(rs.getString("C_TELNO"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			util.close(rs);
+			util.close(stmt);
+			util.close(conn);
+		}
+		
+		return dto;
 	}
+
+	//INSERT INTO BUS_COMPANY(C_CODE, C_NAME, C_ADDRESS, C_TELNO) VALUES('1234', '금호고속', '대전 서구 월평동 123', '01084866687');
 
 	@Override
 	public int insert(BusCompanyDTO dto) {
-		// TODO Auto-generated method stub
+		Connection conn = util.getConnection();
+		PreparedStatement pstmt = null;
+		int res = 0;
+		
+		String sql = "INSERT INTO BUS_COMPANY(C_CODE, C_NAME, C_ADDRESS, C_TELNO) VALUES()";
 		return 0;
 	}
 
